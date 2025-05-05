1826d84d77b6c5c2f63924036283f4383bf16c49
perkinson1251 <tttrashutin@gmail.com>
2025-05-05 11:51:25 +0300

    feat: add shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
-	modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@vueuse/nuxt"],
+	modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@vueuse/nuxt", "shadcn-nuxt"],
+	future: {
+		compatibilityVersion: 4,
+	},
+	css: ["~/core/assets/css/main.css"],
+	shadcn: {
+		prefix: "",
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	icon: {
 		provider: "iconify",
 		serverBundle: {
