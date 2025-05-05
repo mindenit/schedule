70acaac6ac6dee9a1191d91efb37a1de80f197da
perkinson1251 <tttrashutin@gmail.com>
2025-05-05 12:36:45 +0300

    chore: lint code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,31 @@
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import prettier from "eslint-config-prettier"
+import withNuxt from "./.nuxt/eslint.config.mjs"
 
-export default [
+export default withNuxt(
 	{
 		files: ["**/**/*.{js,ts,vue,mjs,mts}"],
 		rules: {
 			"vue/multi-word-component-names": "off",
 			"vue/require-default-prop": "off",
-			"vue/no-v-html": "off",
 		},
+		ignores: [
+			"dist",
+			".nuxt",
+			"node_modules",
+			".output",
+			".git",
+			"build",
+			"LICENSE",
+			"*.md",
+			"public",
+			"package-lock.json",
+		],
+		...prettier,
 	},
-	...eslintPluginPrettierRecommended,
-]
+	{
+		files: ["**/*.vue"],
+		rules: {
+			"vue/require-default-prop": "off",
+		},
+	}
+)
