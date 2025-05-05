9299086952f82f83616a9c7d31d8078a6e83d86d
perkinson1251 <tttrashutin@gmail.com>
2025-05-05 11:29:52 +0300

    chore: add basic modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
-	modules: ["@nuxt/eslint", "@nuxt/icon"],
+	modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@vueuse/nuxt"],
+	icon: {
+		provider: "iconify",
+		serverBundle: {
+			collections: ["lucide"],
+		},
+	},
+	colorMode: {
+		preference: "system",
+		fallback: "light",
+		classSuffix: "",
+		storageKey: "nuxt-color-mode",
+	},
 })
