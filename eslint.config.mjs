931cb5e776b26fd7588b931d9d7881e50bed709d
perkinson1251 <tttrashutin@gmail.com>
2025-05-05 11:56:43 +0300

    chore: fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 export default [
-	eslintPluginPrettierRecommended,
 	{
 		files: ["**/**/*.{js,ts,vue,mjs,mts}"],
 		rules: {
 			"vue/multi-word-component-names": "off",
 			"vue/require-default-prop": "off",
+			"vue/no-v-html": "off",
 		},
 	},
+	...eslintPluginPrettierRecommended,
 ]
