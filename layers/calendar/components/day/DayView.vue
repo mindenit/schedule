797d10cfad9532b8194735b8a0f697d2f0943325
perkinson1251 <tttrashutin@gmail.com>
2025-06-12 23:54:59 +0300

    feat: update styles of day view

--- layers/calendar/components/day/DayView.vue
+++ layers/calendar/components/day/DayView.vue
@@ -29,14 +29,15 @@ function formatHour(hour: number): string {
 </script>
 
 <template>
-	<div class="flex h-full overflow-hidden">
+	<div class="flex h-full overflow-hidden rounded-lg">
 		<div class="flex flex-1 flex-col overflow-hidden">
-			<div class="flex min-h-0 flex-1 overflow-hidden">
+			<div class="flex min-h-0 flex-1 gap-1 overflow-hidden">
 				<div class="relative flex w-18 flex-shrink-0 flex-col">
 					<div
 						v-for="(hour, index) in hours"
 						:key="hour"
-						class="border-border/30 relative flex min-h-[48px] flex-1 items-start justify-end border-b pr-2"
+						class="bg-card relative flex flex-1 items-start justify-end pr-2"
+						:style="{ minHeight: `${100 / hours.length}%` }"
 					>
 						<span v-if="index !== 0" class="text-muted-foreground mt-1 text-xs">
 							{{ formatHour(hour) }}
@@ -44,19 +45,18 @@ function formatHour(hour: number): string {
 					</div>
 				</div>
 
-				<div class="relative flex-1 overflow-hidden border-l">
-					<div class="flex h-full flex-col">
+				<div class="relative flex-1 overflow-hidden">
+					<div class="flex h-full flex-col gap-1">
 						<div
 							v-for="hour in hours"
 							:key="hour"
-							class="border-border/30 relative min-h-[48px] flex-1 border-b"
+							class="bg-card relative min-h-[48px]"
+							:style="{ height: `${(100 - (hours.length - 1) * 0.25) / hours.length}%` }"
 						></div>
 					</div>
-
 					<div class="absolute inset-0">
 						<CalendarEventRenderer :grouped-events="groupedEvents" :day="selectedDate" />
 					</div>
-
 					<CalendarTimeline />
 				</div>
 			</div>
