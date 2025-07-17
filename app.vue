a777ee8e355ee8805afb43e9bcfa4cb606edb932
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-07-18 01:12:13 +0300

    chore: add analytics script

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script lang="ts" setup>
+useHead({
+	script: [
+    {
+      src: 'https://analytics.mindenit.org/api/script.js',
+      defer: true,
+      'data-site-id': '4',
+			'data-tracking-errors': 'true',
+			'data-session-replay': 'true',
+    },
+  ],
+})
+</script>
+
 <template>
 	<NuxtRouteAnnouncer />
 	<NuxtLayout>
