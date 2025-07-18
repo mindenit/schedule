7df4d40680fa7cb134cb5f3e98795faa963a947d
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-07-18 23:52:35 +0300

    fix: add animation

--- layers/calendar/components/day/DayView.vue
+++ layers/calendar/components/day/DayView.vue
@@ -25,53 +25,68 @@ const groupedEvents = computed(() => groupEvents(dayEvents.value))
 
 const dayViewEl = useTemplateRef("dayView")
 
+const swipeAnimation = ref<"swipe-left" | "swipe-right" | null>(null)
+
 const { direction, isSwiping } = useSwipe(dayViewEl)
 
 watch(isSwiping, (swiping) => {
 	if (swiping && direction.value === "right") {
+		swipeAnimation.value = "swipe-right"
 		const newDate = navigateDate(selectedDate.value, 'day', "previous")
 		calendarStore.setSelectedDate(newDate)
 	}
 
 	if (swiping && direction.value === "left") {
+		swipeAnimation.value = "swipe-left"
 		const newDate = navigateDate(selectedDate.value, 'day', "next")
 		calendarStore.setSelectedDate(newDate)
+	}
+
+	if (!swiping) {
+		setTimeout(() => {
+			swipeAnimation.value = null
+		}, 247)
 	}
 })
 </script>
 
 <template>
 	<div ref="dayView" class="flex h-full overflow-hidden md:rounded-lg">
-		<div class="flex flex-1 flex-col overflow-hidden">
-			<div class="flex min-h-0 flex-1 gap-1 overflow-hidden">
-				<div class="relative flex w-18 flex-shrink-0 flex-col">
-					<div
-						v-for="(hour, index) in hours"
-						:key="hour"
-						class="bg-muted/50 relative flex flex-1 items-start justify-end pr-2"
-						:style="{ minHeight: `${100 / hours.length}%` }"
-					>
-						<span v-if="index !== 0" class="text-muted-foreground text-xs">
-							{{ formatHour(hour) }}
-						</span>
-					</div>
-				</div>
-
-				<div class="relative flex-1 overflow-hidden">
-					<div class="flex h-full flex-col gap-1">
+		<Transition
+			:enter-active-class="swipeAnimation === 'swipe-left' ? 'animate-swipe-left' : 'animate-swipe-right'"
+      :leave-active-class="swipeAnimation === 'swipe-left' ? 'animate-swipe-left' : 'animate-swipe-right'"
+		>
+			<div v-if="!swipeAnimation" class="flex flex-1 flex-col overflow-hidden">
+				<div class="flex min-h-0 flex-1 gap-1 overflow-hidden">
+					<div class="relative flex w-18 flex-shrink-0 flex-col">
 						<div
-							v-for="hour in hours"
+							v-for="(hour, index) in hours"
 							:key="hour"
-							class="bg-card relative min-h-[52px]"
-							:style="{ height: `${(100 - (hours.length - 1) * 0.25) / hours.length}%` }"
-						></div>
+							class="bg-muted/50 relative flex flex-1 items-start justify-end pr-2"
+							:style="{ minHeight: `${100 / hours.length}%` }"
+						>
+							<span v-if="index !== 0" class="text-muted-foreground text-xs">
+								{{ formatHour(hour) }}
+							</span>
+						</div>
 					</div>
-					<div class="absolute inset-0">
-						<BigCalendarEventRenderer :grouped-events="groupedEvents" :day="selectedDate" />
+
+					<div class="relative flex-1 overflow-hidden">
+						<div class="flex h-full flex-col gap-1">
+							<div
+								v-for="hour in hours"
+								:key="hour"
+								class="bg-card relative min-h-[52px]"
+								:style="{ height: `${(100 - (hours.length - 1) * 0.25) / hours.length}%` }"
+							></div>
+						</div>
+						<div class="absolute inset-0">
+							<BigCalendarEventRenderer :grouped-events="groupedEvents" :day="selectedDate" />
+						</div>
+						<BigCalendarTimeline v-if="isToday" />
 					</div>
-					<BigCalendarTimeline v-if="isToday" />
 				</div>
 			</div>
-		</div>
+		</Transition>
 	</div>
 </template>
