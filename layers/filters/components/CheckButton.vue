1984e059ad1110a93bfc85f360f9bb820aab19da
perkinson1251 <tttrashutin@gmail.com>
2025-07-28 13:46:45 +0300

    feat: add accordion to filters

---
+++ layers/filters/components/CheckButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+	label: string
+	isActive: boolean
+}
+
+defineProps<Props>()
+
+const emit = defineEmits<{
+	toggle: []
+}>()
+</script>
+
+<template>
+	<Button
+		:variant="isActive ? 'default' : 'outline'"
+		size="sm"
+		class="h-8 text-xs"
+		@click="emit('toggle')"
+	>
+		<Icon v-if="isActive" name="lucide:check" class="!size-3" />
+		{{ label }}
+	</Button>
+</template>
