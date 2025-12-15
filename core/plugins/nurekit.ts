7469f4f15c08d0cfd72c45d5b1e9e5ad43851ba6
perkinson1251 <tttrashutin@gmail.com>
2025-12-15 13:22:46 +0200

    feat: add API proxy handler for dynamic request forwarding

--- core/plugins/nurekit.ts
+++ core/plugins/nurekit.ts
@@ -1,7 +1,21 @@
 import { Nurekit } from "nurekit"
 
 export default defineNuxtPlugin(() => {
-	const nurekit = new Nurekit()
+	let nurekit: Nurekit
+
+	if (process.env.NODE_ENV === "development") {
+		let baseUrl: string
+		if (import.meta.server) {
+			const host = process.env.NUXT_HOST || "localhost"
+			const port = process.env.NUXT_PORT || "3000"
+			baseUrl = `http://${host}:${port}/api`
+		} else {
+			baseUrl = `${window.location.origin}/api`
+		}
+		nurekit = new Nurekit(baseUrl)
+	} else {
+		nurekit = new Nurekit()
+	}
 
 	return {
 		provide: {
